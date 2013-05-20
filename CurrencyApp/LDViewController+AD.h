717705b15b49b758649a4dd3ec0fbaff418ceb70
wangcash <wangcash@gmail.com>
2013-05-21 04:54:01 +0800

    增加NavBar的广告图片

---
+++ CurrencyApp/LDViewController+AD.h
@@ -0,0 +1,14 @@
+//
+//  LDViewController+AD.h
+//  CurrencyApp
+//
+//  Created by Cash on 13-5-21.
+//  Copyright (c) 2013年 LYFORD INTERNATIONAL TRADING PORT PTY.LTD. All rights reserved.
+//
+
+#import "LDViewController.h"
+#import "LDHelper.h"
+
+@interface LDViewController (AD)
+
+@end
